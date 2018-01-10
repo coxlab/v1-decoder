2c3f53e18f4fda6b523f67e8037bfddec120bd13
Joel Dapello <dapello@mit.edu>
2018-01-10 17:49:56 -0500

    cleaned up code, refactored config and pulled data_helper functions out.

--- tempConv.py
+++ tempConv.py
@@ -3,45 +3,29 @@ from keras.layers import Conv1D, Dense, MaxPooling1D, Flatten, Dropout
 from keras.models import Sequential
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
-from data_helpers import format_timeseries
 from metrics_helper import do_the_thing
 
 class tempConvDecoder(object):
     """A class that stores the network, trains it, and determines it's goodness of fit."""
 
-    def __init__(
-        self, timeseries1, timeseries2, key, bs=256, eps=25, lr=0.0005, kernel=2, percent_data=1.0,
-        resample_data=False, sample_size=40000, nb_filter=5, window=30, offset=10, regressor=True, 
-        pyramidal=True, id=None, nb_trains=1, verbose=False
-    ):
-        self.bs = bs
-        self.eps = eps
-        self.lr = lr
-        self.kernel = kernel
-        self.nb_filter = nb_filter
-        self.window = window
-        self.offset = offset
-        self.regressor = regressor
-        self.pyramidal = pyramidal
-        self.id = id
-        self.nb_trains = nb_trains
-        self.verbose = verbose
-        self.key = key
-        self.timeseries1 = timeseries1
-        self.nb_input_samples, self.nb_input_series = timeseries1.shape
-        if timeseries2.ndim == 1: timeseries2 = np.atleast_2d(timeseries2).T
-        self.timeseries2 = timeseries2
-        self.nb_output_samples, self.nb_output_series = timeseries2.shape
-        self.X_train, self.X_test, self.y_train, self.y_test = format_timeseries(
-            timeseries1,
-            timeseries2, 
-            window, 
-            offset,
-            percent_data=percent_data,
-            resample_data=resample_data,
-            sample_size=sample_size,
-            regressor=regressor
-        )
+    def __init__(self, **kwargs):
+        self.bs  = kwargs['bs']
+        self.eps = kwargs['eps']
+        self.lr  = kwargs['lr']
+        self.kernel = kwargs['kernel']
+        self.nb_filter = kwargs['nb_filter']
+        self.window = kwargs['window']
+        self.regressor = kwargs['regressor']
+        self.pyramidal = kwargs['pyramidal']
+        self.run_id = kwargs['run_id']
+        self.verbose = kwargs['verbose']
+        self.key = kwargs['key']
+        self.timeseries1 = kwargs['dataset']['timeseries1']
+        self.timeseries2 = kwargs['dataset']['timeseries2']
+        self.nb_input_samples, self.nb_input_series = kwargs['dataset']['timeseries1'].shape
+        self.nb_output_samples, self.nb_output_series = kwargs['dataset']['timeseries2'].shape
+        self.X_train, self.y_train = kwargs['dataset']['train'] 
+        self.X_test, self.y_test = kwargs['dataset']['test']
 
     def make_timeseries_regressor(self):
         dilation = [4,2] if self.pyramidal else [1,1]
@@ -58,7 +42,7 @@ class tempConvDecoder(object):
             int(self.nb_filter * dilation[1]),
             kernel_size=int(self.kernel * dilation[1]),
             activation='relu'
-        )) 
+        ))
         model.add(MaxPooling1D())
         ## < dupes
         model.add(Conv1D(
@@ -86,11 +70,11 @@ class tempConvDecoder(object):
         ## < dupes
         adam = Adam(lr=self.lr, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
         
-        if self.regressor: 
-            model.add(Dense(self.nb_output_series, activation='linear')) 
+        if self.regressor:
+            model.add(Dense(self.nb_output_series, activation='linear'))
             model.compile(loss='mae', optimizer=adam, metrics=['mse'])
         else:
-            model.add(Dense(self.nb_output_series, activation='sigmoid')) 
+            model.add(Dense(self.nb_output_series, activation='sigmoid'))
             model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['binary_accuracy'])
  
         return model
@@ -132,7 +116,7 @@ class tempConvDecoder(object):
             self.y_test,
             self.y_test_hat,
             self.key,
-            'temp_conv_results_{}_y:{}'.format(self.id, self.key),
+            'temp_conv_results_{}_y:{}'.format(self.run_id, self.key),
             plot_result=plot_result
         )
 
